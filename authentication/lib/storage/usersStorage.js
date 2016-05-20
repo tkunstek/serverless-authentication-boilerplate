8a9676580e7cc954b63f93f99b255a5c8f342725
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-05-20 08:50:50 +0300

    saveUser function

--- authentication/lib/storage/usersStorage.js
+++ authentication/lib/storage/usersStorage.js
@@ -18,8 +18,18 @@ const saveCognito = (profile) => new Promise((resolve, reject) => {
   }
 });
 
+const saveUser = (profile) => {
+  // just temp switch
+  // Here you can save the profile to DynamoDB if it doesn't already exist
+  // In this example it just makes empty callback to continue and nothing is saved.
+  // profile class: https://github.com/laardee/serverless-authentication/blob/master/src/profile.js
+
+  if (true) {
+    return saveDatabase(profile);
+  }
+  return saveCognito(profile);
+};
 
 exports = module.exports = {
-  saveDatabase,
-  saveCognito
+  saveUser
 };
--- specs-docker.sh
+++ specs-docker.sh
@@ -4,9 +4,9 @@ SERVERLESS_STAGE="dev"
 SERVERLESS_REGION="eu-central-1"
 LOCAL_DDB_ENDPOINT="http://192.168.99.100:8000"
 
-docker-machine start default
-eval "$(docker-machine env default)"
-docker-compose up -d
+# docker-machine start default
+# eval "$(docker-machine env default)"
+# docker-compose up -d
 SERVERLESS_STAGE=$SERVERLESS_STAGE SERVERLESS_REGION=$SERVERLESS_REGION SERVERLESS_PROJECT=$SERVERLESS_PROJECT LOCAL_DDB_ENDPOINT=$LOCAL_DDB_ENDPOINT ./node_modules/.bin/mocha specs/
-docker-compose down
-docker-machine stop default
+# docker-compose down
+# docker-machine stop default
