94f5590574fcc0f91d13a1489bb2b54b7cb06e33
Terrence Kunstek <terrence@kunstek.com>
2017-09-25 11:18:50 -0400

    Integrating mongo

---
+++ authentication/lib/storage/mongo/mongoUser.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+const log = require('../../helpers').log;
+
+let atlas_connection_uri = process.env.MONGODB_ATLAS_CLUSTER_URI;
+let atlas_collection = process.env.MONGODB_COLLECTION;
+let cachedDb = null;
+
+const saveUser = (profile) => {
+    log('save user', profile.id);
+    try {
+        if (cachedDb == null) {
+            console.log('=> connecting to database');
+            MongoClient.connect(atlas_connection_uri, function (err, db) {
+              if(err!=null) {
+                console.error("an error occurred during connect", err);
+                callback(null, JSON.stringify(err));
+              }
+                cachedDb = db;
+                return createDoc(db, jsonContents, callback);
+            });
+        }
+        return cachedDb.collection(atlas_collection).insertOne(jsonContents).promise();
+    }
+    catch (err) {
+        console.error('an error occurred', err);
+    }
+
+};
+
+module.exports = {
+    saveUser
+};
+  
